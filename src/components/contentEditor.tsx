47a21c3eeda5d4aae8078917956e86a87d0c0730
thalha <thalhaz999@gmail.com>
2023-12-13 16:50:21 +0530

    dynamic mention options added

--- src/components/contentEditor.tsx
+++ src/components/contentEditor.tsx
@@ -10,20 +10,28 @@ import {
   useHelpers,
 } from "@remirror/react";
 import { AnyExtension, RemirrorEventListener } from "remirror";
-import { useCallback, useState, useEffect } from "react";
+import {
+  useCallback,
+  useState,
+  useEffect,
+  SetStateAction,
+  JSXElementConstructor,
+  Key,
+  PromiseLikeOfReactNode,
+  ReactElement,
+  ReactNode,
+} from "react";
 import { SAMPLE_DOC } from "./editor";
 import { cx } from "@remirror/core";
 import styles from "@/style";
+import { useForm, useFieldArray } from "react-hook-form";
+import { ErrorMessage } from "@hookform/error-message";
 
-const ALL_USERS = [
-  { id: "Thalha", label: "Thalha" },
-  { id: "Hamsa", label: "Hamsa" },
-  { id: "Jhon", label: "Jhon" },
-  { id: "Koya", label: "Koya" },
-  { id: "Doe", label: "Doe" },
-];
+interface MentionSuggestorProps {
+  users: { id: string; label: string }[];
+}
 
-const MentionSuggestor: React.FC = () => {
+const MentionSuggestor: React.FC<MentionSuggestorProps> = ({ users }) => {
   const [options, setOptions] = useState<MentionAtomNodeAttributes[]>([]);
   const { state, getMenuProps, getItemProps, indexIsHovered, indexIsSelected } =
     useMentionAtom({
@@ -37,9 +45,8 @@ const MentionSuggestor: React.FC = () => {
 
     const searchTerm = state.query.full.toLowerCase();
 
-    const filteredOptions = ALL_USERS.filter((user) =>
-      user.label.toLowerCase().includes(searchTerm)
-    )
+    const filteredOptions = users
+      .filter((user) => user.label.toLowerCase().includes(searchTerm))
       .sort()
       .slice(0, 5);
 
@@ -90,6 +97,47 @@ export function ContentEditor({
   onChange: RemirrorEventListener<AnyExtension>;
 }) {
   const [data, setData] = useState("");
+  const [modal, setModal] = useState(false);
+  const [showInputFields, setShowInputFields] = useState(false);
+
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    mode: "onBlur",
+  });
+
+  const { fields, append, remove } = useFieldArray({
+    name: "fields",
+    control,
+  });
+
+  const [formData, setFormData] = useState({
+    fields: [],
+  });
+
+  const [users, setAllUsers] = useState<{ id: string; label: string }[]>([]);
+
+  const onSubmit = (data: any) => {
+    setFormData(data);
+    setShowInputFields(false);
+    setModal(false);
+
+    setAllUsers((prevUsers) => [
+      ...prevUsers,
+      {
+        id: data.fields[data.fields.length - 1].name,
+        label: data.fields[data.fields.length - 1].name,
+      },
+    ]);
+
+    setAllUsers((updatedUsers) => {
+      console.log(updatedUsers);
+      return updatedUsers;
+    });
+  };
 
   function LoadButton() {
     const { setContent } = useRemirrorContext();
@@ -191,16 +239,286 @@ export function ContentEditor({
     );
   }
 
+  function AddButton() {
+    return (
+      <>
+        <button
+          style={{
+            background: "muted",
+            color: "text",
+            cursor: "pointer",
+            marginLeft: "26px",
+          }}
+          onClick={() => setModal(!modal)}
+        >
+          Add Mentions
+        </button>
+      </>
+    );
+  }
+
   return (
     <div>
       <style>{styles}</style>
       <Remirror manager={manager} state={state} onChange={onChange} autoFocus>
         <EditorComponent />
-        <MentionSuggestor />
+        <MentionSuggestor users={users} />
         <LoadButton />
         <SaveButton />
         <PostLoad />
         <Replace />
+        <AddButton />
+        {modal && (
+          <div
+            style={{
+              width: "50rem",
+              height: "15rem",
+              margin: "auto",
+              background: "#e4e9ef",
+              position: "absolute",
+              top: 3,
+              bottom: 3,
+              left: 3,
+              right: 3,
+              borderRadius: "12px",
+              zIndex: "999",
+            }}
+            onSubmit={(e) => e.preventDefault()}
+          >
+            {!showInputFields ? (
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  padding: "25px 32px 16px",
+                  background: "#e4e9ef",
+                }}
+              >
+                <p
+                  style={{
+                    fontWeight: "bold",
+                  }}
+                >
+                  No Mention Options
+                </p>
+                <p
+                  style={{
+                    fontSize: "12px",
+                    color: "#515b66",
+                  }}
+                >
+                  You have no mention options added, Start Adding now
+                </p>
+              </div>
+            ) : (
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  background: "white",
+                }}
+              >
+                {fields.map((field, index) => (
+                  <>
+                    <div
+                      key={field.id}
+                      style={{
+                        display: "flex",
+                        gap: "20px",
+                        borderBottomWidth: "1px",
+                        borderBottomStyle: "solid",
+                        padding: "16px 32px 24px",
+                        borderColor: "#e4e9ef",
+                      }}
+                    >
+                      <div
+                        style={{
+                          display: "flex",
+                          flexDirection: "column",
+                          flexGrow: "1",
+                        }}
+                      >
+                        <h1
+                          style={{
+                            fontSize: "10px",
+                            paddingBottom: "4px",
+                          }}
+                        >
+                          Field Name
+                        </h1>
+                        <input
+                          style={{
+                            padding: "4px 8px",
+                            width: "100%",
+                            borderColor: "#e4e9ef",
+                            fontSize: "16px",
+                            fontWeight: 400,
+                            border: "1px solid",
+                          }}
+                          {...register(`fields.${index}.name`, {
+                            required: "Name required",
+                          })}
+                        ></input>
+                        <ErrorMessage
+                          errors={errors}
+                          name={`fields.${index}.name`}
+                        />
+                      </div>
+                      <div
+                        style={{
+                          display: "flex",
+                          flexDirection: "column",
+                        }}
+                      >
+                        <h1
+                          style={{
+                            fontSize: "10px",
+                            paddingBottom: "4px",
+                          }}
+                        >
+                          Action
+                        </h1>
+                        <div
+                          style={{
+                            display: "flex",
+                            border: "1px solid #e4e9ef",
+                            width: "33px",
+                            height: "35px",
+                            alignItems: "center",
+                            justifyContent: "center",
+                            borderRadius: "6px",
+                          }}
+                          onClick={() => {
+                            remove(index);
+                          }}
+                        >
+                          <svg
+                            height="1.25rem"
+                            width="1.25rem"
+                            fill="currentColor"
+                            data-color="red"
+                          >
+                            <path d="M5 20a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V8h2V6h-4V4a2 2 0 0 0-2-2H9a2 2 0 0 0-2 2v2H3v2h2zM9 4h6v2H9zM8 8h9v12H7V8z"></path>
+                            <path d="M9 10h2v8H9zm4 0h2v8h-2z"></path>
+                          </svg>
+                        </div>
+                      </div>
+                    </div>
+                  </>
+                ))}
+              </div>
+            )}
+            <div
+              style={{
+                display: "flex",
+                gap: 2,
+                padding: "32px",
+                background: "#e4e9ef",
+              }}
+            >
+              <button
+                style={{
+                  fontWeight: 700,
+                  cursor: "pointer",
+                  fontSize: "14px",
+                  border: "1px solid",
+                  padding: "8px 16px",
+                  background: "grey",
+                  margin: "0px",
+                  borderRadius: "6px",
+                }}
+                onClick={() => {
+                  append({
+                    name: "",
+                  });
+                  setShowInputFields(true);
+                }}
+              >
+                Add Field
+              </button>
+            </div>
+            {showInputFields && (
+              <div
+                style={{
+                  display: "flex",
+                  gap: 2,
+                  justifyContent: "end",
+                  padding: "16px 32px",
+                  background: "#f0ecec",
+                  borderBottomLeftRadius: "12px",
+                  borderBottomRightRadius: "12px",
+                }}
+              >
+                <button
+                  style={{
+                    fontWeight: 700,
+                    cursor: "pointer",
+                    fontSize: "12px",
+                    border: "1px solid",
+                    padding: "8px 16px",
+                    background: "#89a7ff",
+                    margin: "0px",
+                    borderRadius: "6px",
+                  }}
+                  onClick={() => setModal(false)}
+                >
+                  Cancel
+                </button>
+                <button
+                  onClick={handleSubmit(onSubmit)}
+                  style={{
+                    fontWeight: 700,
+                    cursor: "pointer",
+                    fontSize: "12px",
+                    border: "1px solid",
+                    padding: "8px 16px",
+                    background: "#89a7ff",
+                    margin: "0px",
+                    borderRadius: "6px",
+                  }}
+                >
+                  Save
+                </button>
+              </div>
+            )}
+          </div>
+        )}
+
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "flex-start",
+            position: "absolute",
+            left: "1150px",
+            width: "250px",
+          }}
+        >
+          <div
+            style={{
+              borderBottom: "1px solid black",
+              paddingLeft: "10px",
+            }}
+          >
+            <p>Avaliable Mentions</p>
+          </div>
+          {formData.fields.map((field, index) => {
+            return (
+              <div key={index}>
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "center",
+                    border: "1px solid",
+                  }}
+                >
+                  <p>{field.name}</p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </Remirror>
     </div>
   );
